735b40c1e978fcc426b75395f77e6aa8b7d04ae5
SophieDeBenedetto <sophie.debenedetto@gmail.com>
2015-07-03 12:28:00 -0400

    fixing https security issue

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,5 +2,5 @@ import DS from 'ember-data';
 
 export default DS.ActiveModelAdapter.extend({
   namespace: 'api/v1',
-  host: 'http://flatbook-api.herokuapp.com'
+  host: 'https://flatbook-api.herokuapp.com'
 });
