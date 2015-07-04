bde225c189a55f96e06e34d1791ea0a5ffa1f21c
SophieDeBenedetto <sophie.debenedetto@gmail.com>
2015-07-04 16:48:33 -0400

    fix styling

--- app/helpers/short-bio.js
+++ app/helpers/short-bio.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 
 export default Ember.Handlebars.makeBoundHelper(function(bio){
-  if (bio.length > 200){
+  if (bio.length > 200) {
 	var theString = bio.substring(0,200) + " ... ";
 	return new Ember.Handlebars.SafeString(theString);
   }
